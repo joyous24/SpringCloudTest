3abe239f36757184ce55bbc95e3ecf8fc51ceb57
zhaoxq <mascot417@126.com>
2018-05-30 21:49:58 +0800

    构建配置中心

--- configServer/src/main/java/com/zxq/ConfigServerApplication.java
+++ configServer/src/main/java/com/zxq/ConfigServerApplication.java
@@ -2,12 +2,14 @@ package com.zxq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Ribbon负载均衡
  */
 @EnableConfigServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServerApplication {
 
--- helloService/src/main/java/com/zxq/Controller/HelloController.java
+++ helloService/src/main/java/com/zxq/Controller/HelloController.java
@@ -3,12 +3,14 @@ package com.zxq.Controller;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@RefreshScope
 @RestController
 public class HelloController {
 
@@ -17,10 +19,18 @@ public class HelloController {
     @Autowired
     private DiscoveryClient client;
 
+    @Value("${from}")
+    private String from;
+
     @RequestMapping("/hello")
     public String hello() {
         ServiceInstance instance = client.getLocalServiceInstance();
         logger.info("/hello, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
         return "Hello World";
+    }
+
+    @RequestMapping("/from")
+    public String from() {
+        return this.from;
     }
 }
